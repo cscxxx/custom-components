1f66b83f5836cb083790cab828c8d73a5c47764a
cscxxx <167551334+cscxxx@users.noreply.github.com>
2025-02-03 20:34:52 +0800

    添加使用useReducer

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from "react-dom/client";
-import Demo from "./EditableTable/demo.tsx";
+// import Demo from "./EditableTable/demo.tsx";
+import UseReducerTaskApp from "./Reducer/index";
 import React from "react";
 
-createRoot(document.getElementById("container") as HTMLElement).render(<Demo />);
+createRoot(document.getElementById("container") as HTMLElement).render(
+  <UseReducerTaskApp />
+);
